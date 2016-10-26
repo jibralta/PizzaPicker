34fabb7e03ac64b7891f01723e54d46c5bdb4e17
Joy Umali <joyumali@gmail.com>
2016-10-26 15:52:18 -0700

    added files. UI draft done

--- PizzaPicker/AppDelegate.swift
+++ PizzaPicker/AppDelegate.swift
@@ -15,8 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        Thread.sleep(forTimeInterval: 1.5)
         return true
+        
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
